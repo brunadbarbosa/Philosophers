2f2e053481622510f2eaeb3587ff5ab470a4a0fc
Bruna Maria Duarte Barbosa <brmaria-@c1r13s3.42porto.com>
2025-10-22 17:39:23 +0100

    built atoll, and vaidation args

--- philo/main.c
+++ philo/main.c
@@ -6,13 +6,57 @@
 /*   By: brmaria- <brmaria-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 15:44:37 by brmaria-          #+#    #+#             */
-/*   Updated: 2025/10/22 15:50:04 by brmaria-         ###   ########.fr       */
+/*   Updated: 2025/10/22 17:37:03 by brmaria-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "philo.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int	validate_args(int argc, char **argv)
+{
+	if (argc != 5 || argc != 6)
+	{
+		printf("Error: Wrong number of arguments\n");
+		return (0);		
+	}
+	if (!check_character(argv))
+	{
+		printf("Error: Arguments must be positive numbers\n");
+		return (0);
+	}
+	return (1);
+}
+
+long long	ft_atoll(char *s)
+{
+	int			i = 0;
+	int			sign = 1;
+	long long	result = 0;
+
+	while (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' 
+		|| s[i] == '\v' || s[i] == '\f' || s[i] == '\r')
+		i++;
+	if (s[i] == '+' || s[i] == '-')
+	{
+		if (s[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (s[i])
+	{
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
+	return (result * sign);
+}
 int main(int argc, char **argv)
 {
-	if (argc > 6 || argc < 5)
+	//t_rules	*rules;
+	
+	if (!validate_args(argc, argv))
 		return (1);
-	char	**
+	printf("Passou no valid0");
+	return (0);
 }
--- philo/philo.h
+++ philo/philo.h
@@ -6,14 +6,21 @@
 /*   By: brmaria- <brmaria-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 15:28:32 by brmaria-          #+#    #+#             */
-/*   Updated: 2025/10/22 15:48:09 by brmaria-         ###   ########.fr       */
+/*   Updated: 2025/10/22 16:53:17 by brmaria-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
-
+typedef struct s_rules
+{
+	int			num_philos;
+	long long	time_to_die;
+	long long	time_to_eat;
+	long long	time_to_sleep;
+	int			meals_limit;
+}	t_rules;
 
 
 
