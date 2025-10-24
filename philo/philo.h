ff99f3d4fbbb3fec10574d4bec98b30d56ea0332
Bruna Maria Duarte Barbosa <brmaria-@c1r13s1.42porto.com>
2025-10-24 18:12:43 +0100

    Threads built and a test function added to check

--- philo/philo.h
+++ philo/philo.h
@@ -6,12 +6,16 @@
 /*   By: brmaria- <brmaria-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 15:28:32 by brmaria-          #+#    #+#             */
-/*   Updated: 2025/10/22 17:48:16 by brmaria-         ###   ########.fr       */
+/*   Updated: 2025/10/24 16:16:08 by brmaria-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
 
 typedef struct s_rules
 {
@@ -22,6 +26,13 @@ typedef struct s_rules
 	int			meals_limit;
 }	t_rules;
 
+typedef struct s_philosopher
+{
+	int			id;
+	long long	last_meal;
+	int			meals_eaten;
+	t_rules		*rules;
+} t_philosopher;
 
 int	validate_args(int argc, char **argv);
 long long	ft_atoll(char *s);
